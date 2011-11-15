930d9f3b72ffb7a5b5233f6d5ef9218bc62d27ec
Otto von Wesendonk <wesendon@in.tum.de>
2011-11-15 10:57:19 +0100

    added new update handler

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/commands/handler/UpdateProjectHandler.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/commands/handler/UpdateProjectHandler.java
@@ -1,12 +1,6 @@
 package org.eclipse.emf.emfstore.client.ui.commands.handler;
 
-import org.eclipse.emf.emfstore.client.model.ProjectSpace;
-import org.eclipse.emf.emfstore.client.model.Usersession;
-import org.eclipse.emf.emfstore.client.model.WorkspaceManager;
 import org.eclipse.emf.emfstore.client.model.util.EMFStoreCommand;
-import org.eclipse.emf.emfstore.server.exceptions.AccessControlException;
-import org.eclipse.emf.emfstore.server.exceptions.EmfStoreException;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 public class UpdateProjectHandler extends AbstractEMFStoreHandler {
 
@@ -16,38 +10,7 @@ public class UpdateProjectHandler extends AbstractEMFStoreHandler {
 			@Override
 			protected void doRun() {
 
-				/**
-				 * TODO ASYNC
-				 * OLD STYLE CODE - REPLACE THIS
-				 */
-				ProjectSpace projectSpace = getProjectSpace();
-				if (projectSpace == null) {
-					ProjectSpace activeProjectSpace = WorkspaceManager.getInstance().getCurrentWorkspace()
-						.getActiveProjectSpace();
-					if (activeProjectSpace == null) {
-						MessageDialog.openInformation(getShell(), "Information", "You must select the Project");
-						return;
-					}
-					projectSpace = activeProjectSpace;
-				}
-				Usersession usersession = projectSpace.getUsersession();
-				if (usersession == null) {
-					MessageDialog.openInformation(getShell(), null,
-						"This project is not yet shared with a server, you cannot update.");
-					return;
-				}
-				try {
-					usersession.logIn();
-				} catch (AccessControlException e) {
-					e.printStackTrace();
-				} catch (EmfStoreException e) {
-					e.printStackTrace();
-				}
-				/**
-				 * END OLD STYLE
-				 */
-
-				new UpdateProjectUIController(getShell()).update(projectSpace, null);
+				new UpdateProjectUIController(getShell()).update(getProjectSpace(), null);
 
 			}
 		}.run(true);
--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/commands/handler/UpdateProjectUIController.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/commands/handler/UpdateProjectUIController.java
@@ -19,14 +19,16 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
-public class UpdateProjectUIController extends AbstractEMFStoreUIController
-		implements UpdateCallback {
+public class UpdateProjectUIController extends AbstractEMFStoreUIController implements UpdateCallback {
 
 	public UpdateProjectUIController(Shell shell) {
 		this.setShell(shell);
 	}
 
 	public void update(ProjectSpace projectSpace, VersionSpec version) {
+
+		// TODO sanity check projectspace (is null, is shared)
+
 		progressDialog = getProgressMonitorDialog();
 		projectSpace.update(version, this, progressDialog.getProgressMonitor());
 	}
@@ -45,46 +47,37 @@ public class UpdateProjectUIController extends AbstractEMFStoreUIController
 	protected void handleNoChangesException() {
 		closeProgress();
 		MessageDialog.openInformation(getShell(), "No need to update",
-				"Your project is up to date, you do not need to update.");
+			"Your project is up to date, you do not need to update.");
 	}
 
-	protected void handleChangeConflictException(
-			ChangeConflictException conflictException) {
+	protected void handleChangeConflictException(ChangeConflictException conflictException) {
 		closeProgress();
 		ProjectSpace projectSpace = conflictException.getProjectSpace();
 		try {
-			PrimaryVersionSpec targetVersion = projectSpace
-					.resolveVersionSpec(VersionSpec.HEAD_VERSION);
-			projectSpace.merge(targetVersion, new MergeProjectHandler(
-					conflictException));
+			PrimaryVersionSpec targetVersion = projectSpace.resolveVersionSpec(VersionSpec.HEAD_VERSION);
+			projectSpace.merge(targetVersion, new MergeProjectHandler(conflictException));
 		} catch (EmfStoreException e) {
-			WorkspaceUtil
-					.logException("Exception when merging the project!", e);
+			WorkspaceUtil.logException("Exception when merging the project!", e);
 		}
 	}
 
-	public boolean inspectChanges(ProjectSpace projectSpace,
-			List<ChangePackage> changePackages) {
-		UpdateDialog updateDialog = new UpdateDialog(getShell(), projectSpace,
-				changePackages);
+	public boolean inspectChanges(ProjectSpace projectSpace, List<ChangePackage> changePackages) {
+		UpdateDialog updateDialog = new UpdateDialog(getShell(), projectSpace, changePackages);
 		if (updateDialog.open() == Window.OK) {
 			return true;
 		}
 		return false;
 	}
 
-	public void updateCompleted(ProjectSpace projectSpace,
-			PrimaryVersionSpec oldVersion, PrimaryVersionSpec newVersion) {
+	public void updateCompleted(ProjectSpace projectSpace, PrimaryVersionSpec oldVersion, PrimaryVersionSpec newVersion) {
 		WorkspaceUtil.logUpdate(projectSpace, oldVersion, newVersion);
 		// explicitly refresh the decorator since no simple attribute has
 		// been changed
 		// (as opposed to committing where the dirty property is being set)
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
-				PlatformUI
-						.getWorkbench()
-						.getDecoratorManager()
-						.update("org.eclipse.emf.emfstore.client.ui.decorators.VersionDecorator");
+				PlatformUI.getWorkbench().getDecoratorManager()
+					.update("org.eclipse.emf.emfstore.client.ui.decorators.VersionDecorator");
 			}
 		});
 		closeProgress();
