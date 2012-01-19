27ce0f2a075ab61082427d7670b40d9f553f666c
Edgar Mueller <edgar.m.mueller@gmail.com>
2012-01-19 14:59:05 +0100

    - javadoc - fixed EMFStoreBrowser label provider

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/handlers/RequiredSelectionException.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/handlers/RequiredSelectionException.java
@@ -1,5 +1,12 @@
 package org.eclipse.emf.emfstore.client.ui.handlers;
 
+/**
+ * Indicates that a required selection is missing.
+ * 
+ * @author emueller
+ */
 public class RequiredSelectionException extends RuntimeException {
+
+	private static final long serialVersionUID = 3011252354930520148L;
 
 }
--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/emfstorebrowser/provider/ESBrowserLabelProvider.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/emfstorebrowser/provider/ESBrowserLabelProvider.java
@@ -12,84 +12,67 @@ package org.eclipse.emf.emfstore.client.ui.views.emfstorebrowser.provider;
 
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.emf.emfstore.client.model.ServerInfo;
+import org.eclipse.emf.emfstore.client.ui.Activator;
+import org.eclipse.emf.emfstore.server.model.ProjectInfo;
+import org.eclipse.swt.graphics.Image;
 
 /**
- * Label provider for the ESBrowser TreeViewer.
+ * Label provider for the EMFStore Browser View.
  * 
- * @author shterev
+ * @author emueller
+ * @author ovonwesend
  */
 public class ESBrowserLabelProvider extends AdapterFactoryLabelProvider {
 
+	/**
+	 * Default constructor.
+	 */
 	public ESBrowserLabelProvider() {
 		super(new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
 	}
 
-	// /**
-	// * {@inheritDoc}
-	// */
-	// @Override
-	// public String getText(Object obj) {
-	// Object element = obj;
-	// if (element instanceof ServerInfo) {
-	// ServerInfo serverInfo = (ServerInfo) element;
-	// StringBuilder builder = new StringBuilder();
-	// builder.append(serverInfo.getUrl());
-	// builder.append(" [");
-	// builder.append(serverInfo.getName());
-	// builder.append("]");
-	// return builder.toString();
-	// } else if (element instanceof ProjectInfo) {
-	// ProjectInfo projectInfo = (ProjectInfo) element;
-	// return projectInfo.getName();
-	// }
-	//
-	// return super.getText(obj);
-	// }
-	//
-	// /**
-	// * {@inheritDoc}
-	// */
-	// @Override
-	// public Image getImage(Object obj) {
-	//
-	// Object element = obj;
-	// if (element instanceof ServerInfo) {
-	// return Activator.getImageDescriptor("icons/ServerInfo.gif").createImage();
-	// } else if (element instanceof ProjectInfo) {
-	// return Activator.getImageDescriptor("icons/prj_obj.gif").createImage();
-	// }
-	//
-	// return super.getImage(obj);
-	// }
-	//
-	// /**
-	// * {@inheritDoc}
-	// */
-	// @Override
-	// public void update(ViewerCell cell) {
-	// Object obj = cell.getElement();
-	//
-	// Object element = obj;
-	// if (element instanceof ServerInfo) {
-	// ServerInfo serverInfo = (ServerInfo) element;
-	// StyledString styledString = new StyledString(serverInfo.getName());
-	// String url = serverInfo.getUrl();
-	// styledString.append(" [" + url + "]", StyledString.DECORATIONS_STYLER);
-	//
-	// cell.setText(styledString.toString());
-	// cell.setStyleRanges(styledString.getStyleRanges());
-	//
-	// cell.setImage(Activator.getImageDescriptor("icons/ServerInfo.gif").createImage());
-	// } else if (element instanceof ProjectInfo) {
-	// ProjectInfo projectInfo = (ProjectInfo) element;
-	// StyledString styledString = new StyledString(projectInfo.getName());
-	// cell.setText(styledString.toString());
-	// cell.setStyleRanges(styledString.getStyleRanges());
-	//
-	// cell.setImage(Activator.getImageDescriptor("icons/prj_obj.gif").createImage());
-	// }
-	//
-	// super.update(cell);
-	// }
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider#getText(java.lang.Object)
+	 */
+	@Override
+	public String getText(Object obj) {
+		Object element = obj;
+		if (element instanceof ServerInfo) {
+			ServerInfo serverInfo = (ServerInfo) element;
+			StringBuilder builder = new StringBuilder();
+			builder.append(serverInfo.getUrl());
+			builder.append(" [");
+			builder.append(serverInfo.getName());
+			builder.append("]");
+			return builder.toString();
+		} else if (element instanceof ProjectInfo) {
+			ProjectInfo projectInfo = (ProjectInfo) element;
+			return projectInfo.getName();
+		}
 
+		return super.getText(obj);
+	}
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider#getImage(java.lang.Object)
+	 */
+	@Override
+	public Image getImage(Object obj) {
+
+		Object element = obj;
+		if (element instanceof ServerInfo) {
+			return Activator.getImageDescriptor("icons/ServerInfo.gif").createImage();
+		} else if (element instanceof ProjectInfo) {
+			return Activator.getImageDescriptor("icons/prj_obj.gif").createImage();
+		}
+
+		return super.getImage(obj);
+	}
 }
