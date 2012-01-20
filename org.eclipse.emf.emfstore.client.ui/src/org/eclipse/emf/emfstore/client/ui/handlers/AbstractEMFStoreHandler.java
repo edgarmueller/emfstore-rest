b48da19b36825cef8184572320a83fa987fcb6e6
Edgar Mueller <edgar.m.mueller@gmail.com>
2012-01-20 12:18:52 +0100

    added automated exception handling for handlers

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/handlers/AbstractEMFStoreHandler.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/handlers/AbstractEMFStoreHandler.java
@@ -1,12 +1,24 @@
 package org.eclipse.emf.emfstore.client.ui.handlers;
 
+import org.eclipse.emf.emfstore.client.ui.util.EMFStoreMessageDialog;
+import org.eclipse.emf.emfstore.server.exceptions.EmfStoreException;
+
 public abstract class AbstractEMFStoreHandler extends AbstractEMFStoreHandlerWithResult<Object> {
 
 	@Override
 	public Object handleWithResult() {
-		handle();
+		try {
+			handle();
+		} catch (Exception e) {
+			handleException(e);
+		}
 		return null;
 	}
 
-	public abstract void handle();
+	public abstract void handle() throws EmfStoreException;
+
+	public void handleException(Exception exception) {
+		EMFStoreMessageDialog.showExceptionDialog(exception);
+	}
+
 }
