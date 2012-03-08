7d9a1dd967f8542e9ec9f94b016254173f211e8a
Edgar Mueller <edgar.m.mueller@gmail.com>
2012-03-08 17:46:24 +0100

    made OperationRecorderListener inherit from IObserver

--- org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/client/model/impl/OperationRecorderListener.java
+++ org.eclipse.emf.emfstore.client/src/org/eclipse/emf/emfstore/client/model/impl/OperationRecorderListener.java
@@ -2,9 +2,10 @@ package org.eclipse.emf.emfstore.client.model.impl;
 
 import java.util.List;
 
+import org.eclipse.emf.emfstore.common.observer.IObserver;
 import org.eclipse.emf.emfstore.server.model.versioning.operations.AbstractOperation;
 
-public interface OperationRecorderListener {
+public interface OperationRecorderListener extends IObserver {
 
 	void operationsRecorded(List<? extends AbstractOperation> operations);
 
