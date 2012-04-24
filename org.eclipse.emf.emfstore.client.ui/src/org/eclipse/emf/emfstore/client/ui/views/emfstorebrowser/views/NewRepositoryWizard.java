87a7149978a55d04e3e112ef5cc72b2677ff8103
mhohenegger <mhohenegger@eclipsesource.com>
2012-04-24 10:26:22 +0200

    removed unused variable

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/emfstorebrowser/views/NewRepositoryWizard.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/emfstorebrowser/views/NewRepositoryWizard.java
@@ -31,8 +31,6 @@ public class NewRepositoryWizard extends Wizard implements INewWizard {
 
 	private NewRepositoryWizardPageOne mainPage;
 
-	private boolean edit;
-
 	/**
 	 * Default constructor.
 	 */
