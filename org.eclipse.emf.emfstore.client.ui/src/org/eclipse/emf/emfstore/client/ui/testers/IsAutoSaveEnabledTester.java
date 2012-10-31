6e350c8b173284d500d3884854fa3192494846c7
Edgar Mueller <edgar.m.mueller@gmail.com>
2012-10-31 19:44:52 +0100

    pragmatic solution to prevent unconditional contribution of the save button to the toolbar

--- org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/testers/IsAutoSaveEnabledTester.java
+++ org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/testers/IsAutoSaveEnabledTester.java
@@ -21,6 +21,11 @@ import org.eclipse.emf.emfstore.client.model.Configuration;
  */
 public class IsAutoSaveEnabledTester extends PropertyTester {
 
+	// TODO: think about a more elegant solution to prevent
+	// unconditional contribution of the save button to the toolbar,
+	// e.g. by means of parameters or the like
+	private static boolean isAutoSaveEnabledTesterEnabled = true;
+
 	/**
 	 * {@inheritDoc}
 	 * 
@@ -28,7 +33,28 @@ public class IsAutoSaveEnabledTester extends PropertyTester {
 	 *      java.lang.Object)
 	 */
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		return (expectedValue != null && expectedValue.equals(Configuration.isAutoSaveEnabled()));
+		return (expectedValue != null && expectedValue.equals(Configuration.isAutoSaveEnabled()
+			&& isAutoSaveEnabledTesterEnabled));
 	}
 
+	/**
+	 * Whether the tester itself is enabled.
+	 * The tester may be disabled, if the the contribution of the save button
+	 * that is enabled when autosave is disabled, should be prevented.
+	 * 
+	 * @return whether the tester is enabled
+	 */
+	public static boolean isEnabled() {
+		return isAutoSaveEnabledTesterEnabled;
+	}
+
+	/**
+	 * Enables or disables the tester.
+	 * 
+	 * @param value
+	 *            the enabled state of the tester
+	 */
+	public static void setEnabled(boolean value) {
+		isAutoSaveEnabledTesterEnabled = value;
+	}
 }
