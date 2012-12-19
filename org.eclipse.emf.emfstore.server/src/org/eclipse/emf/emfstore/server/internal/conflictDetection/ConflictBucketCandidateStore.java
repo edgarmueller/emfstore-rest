9f2fae4b2c391fa0716137fa3a81c3d82c630c83
Edgar Mueller <edgar.m.mueller@gmail.com>
2012-12-19 18:43:52 +0100

    added headers

--- org.eclipse.emf.emfstore.server/src/org/eclipse/emf/emfstore/server/internal/conflictDetection/ConflictBucketCandidateStore.java
+++ org.eclipse.emf.emfstore.server/src/org/eclipse/emf/emfstore/server/internal/conflictDetection/ConflictBucketCandidateStore.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource Muenchen GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ ******************************************************************************/
 package org.eclipse.emf.emfstore.server.internal.conflictDetection;
 
 import org.eclipse.emf.emfstore.server.conflictDetection.ConflictBucketCandidate;
