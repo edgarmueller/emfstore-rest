08d07d964c4714bc4039be037c819d276dd5c81b
eneufeld <neufeld.eugen@googlemail.com>
2013-02-07 17:53:22 +0100

    updated test by tobi

--- org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/api/ModelElementTest.java
+++ org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/api/ModelElementTest.java
@@ -22,6 +22,7 @@ import org.eclipse.emf.emfstore.bowling.Player;
 import org.eclipse.emf.emfstore.bowling.Tournament;
 import org.eclipse.emf.emfstore.client.api.ILocalProject;
 import org.eclipse.emf.emfstore.client.api.IWorkspaceProvider;
+import org.eclipse.emf.emfstore.client.model.util.EMFStoreCommand;
 import org.junit.Test;
 
 /**
@@ -35,7 +36,7 @@ public class ModelElementTest {
 	 * Tests adding model elements.
 	 */
 	@Test
-	public void testAddModelElements() {
+	public void testAddModelElementsWithoutCommands() {
 		ILocalProject localProject = IWorkspaceProvider.INSTANCE.getWorkspace().createLocalProject(
 			"Testprojekt", "");
 
@@ -90,14 +91,122 @@ public class ModelElementTest {
 	}
 
 	/**
+	 * Tests adding model elements.
+	 */
+	@Test
+	public void testAddModelElementsWithCommands() {
+		final ILocalProject localProject = IWorkspaceProvider.INSTANCE.getWorkspace().createLocalProject(
+			"Testprojekt", "");
+
+		final League leagueA = ProjectChangeUtil.createLeague("America");
+		final League leagueB = ProjectChangeUtil.createLeague("Europe");
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				localProject.getModelElements().add(leagueA);
+				localProject.getModelElements().add(leagueB);
+			}
+		}.execute();
+
+		assertEquals(2, localProject.getAllModelElements().size());
+
+		final Player playerA = ProjectChangeUtil.createPlayer("Hans");
+		final Player playerB = ProjectChangeUtil.createPlayer("Anton");
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				leagueA.getPlayers().add(playerA);
+				leagueA.getPlayers().add(playerB);
+			}
+		}.execute();
+
+		assertEquals(4, localProject.getAllModelElements().size());
+
+		final Player playerC = ProjectChangeUtil.createPlayer("Paul");
+		final Player playerD = ProjectChangeUtil.createPlayer("Klaus");
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				leagueA.getPlayers().add(playerC);
+				leagueA.getPlayers().add(playerD);
+			}
+		}.execute();
+
+		assertEquals(6, localProject.getAllModelElements().size());
+		assertEquals(2, localProject.getModelElements().size());
+
+		final Tournament tournamentA = ProjectChangeUtil.createTournament(false);
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				localProject.getModelElements().add(tournamentA);
+			}
+		}.execute();
+
+		final Matchup matchupA = ProjectChangeUtil.createMatchup(null, null);
+		final Matchup matchupB = ProjectChangeUtil.createMatchup(null, null);
+
+		final Game gameA = ProjectChangeUtil.createGame(playerA);
+		final Game gameB = ProjectChangeUtil.createGame(playerB);
+		final Game gameC = ProjectChangeUtil.createGame(playerC);
+		final Game gameD = ProjectChangeUtil.createGame(playerD);
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				tournamentA.getMatchups().add(matchupA);
+				matchupA.getGames().add(gameA);
+				matchupA.getGames().add(gameB);
+			}
+		}.execute();
+
+		assertEquals(10, localProject.getAllModelElements().size());
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				tournamentA.getMatchups().add(matchupB);
+				matchupB.getGames().add(gameC);
+				matchupB.getGames().add(gameD);
+			}
+		}.execute();
+
+		assertEquals(13, localProject.getAllModelElements().size());
+		assertEquals(3, localProject.getModelElements().size());
+	}
+
+	/**
 	 * Test deletion of all model elements.
 	 */
 	@Test
-	public void testDeleteAllModelElementsTest() {
-		ILocalProject localProject = ProjectChangeUtil.createBasicBowlingProject();
+	public void testDeleteAllModelElementsWithCommand() {
+		final ILocalProject localProject = ProjectChangeUtil.createBasicBowlingProject();
+
+		new EMFStoreCommand() {
+			@Override
+			protected void doRun() {
+				Set<EObject> elements = localProject.getAllModelElements();
+				for (EObject object : elements) {
+					localProject.getModelElements().remove(object);
+				}
+			}
+		}.execute();
+
+		assertEquals(0, localProject.getAllModelElements());
+	}
+
+	/**
+	 * Test deletion of all model elements.
+	 */
+	@Test
+	public void testDeleteAllModelElementsWithoutCommand() {
+		final ILocalProject localProject = ProjectChangeUtil.createBasicBowlingProject();
 
 		Set<EObject> elements = localProject.getAllModelElements();
-
 		for (EObject object : elements) {
 			localProject.getModelElements().remove(object);
 		}
