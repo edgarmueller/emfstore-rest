5d93c577ed4b9c33eccdd3dadd899a9a80030f1b
eneufeld <neufeld.eugen@googlemail.com>
2013-02-07 12:30:52 +0100

    Tobis LogMessageFactory

--- org.eclipse.emf.emfstore.server.model/src/org/eclipse/emf/emfstore/server/model/api/ILogMessage.java
+++ org.eclipse.emf.emfstore.server.model/src/org/eclipse/emf/emfstore/server/model/api/ILogMessage.java
@@ -1,7 +1,44 @@
+/*******************************************************************************
+ * Copyright 2011 Chair for Applied Software Engineering,
+ * Technische Universitaet Muenchen.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ ******************************************************************************/
 package org.eclipse.emf.emfstore.server.model.api;
 
+import java.util.Date;
+
+/**
+ * A LogMessage.
+ */
 public interface ILogMessage {
 
-	void setMessage(String logMessage);
+	/** Factory for creating ILogMessages. */
+	ILogMessageFactory FACTORY = new ILogMessageFactory();
+
+	/**
+	 * Gets the message.
+	 * 
+	 * @return the message
+	 */
+	String getMessage();
+
+	/**
+	 * Gets the author.
+	 * 
+	 * @return the author
+	 */
+	String getAuthor();
+
+	/**
+	 * Gets the client date.
+	 * 
+	 * @return the client date
+	 */
+	Date getClientDate();
 
 }
