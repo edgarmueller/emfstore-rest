8cc4aae7295d6cb49ef46f150f3aa3c28155eb2e
Edgar Mueller <edgar.m.mueller@gmail.com>
2013-02-13 12:20:42 +0100

    removed ProjectListUpdater as post notify extension point, javadoc

--- org.eclipse.emf.emfstore.server.model/src/org/eclipse/emf/emfstore/server/model/versionspec/IVersionSpec.java
+++ org.eclipse.emf.emfstore.server.model/src/org/eclipse/emf/emfstore/server/model/versionspec/IVersionSpec.java
@@ -2,8 +2,22 @@ package org.eclipse.emf.emfstore.server.model.versionspec;
 
 import org.eclipse.emf.emfstore.internal.server.model.versioning.VersionsFactory;
 
+/**
+ * <p>
+ * Represents a version specifier.
+ * </p>
+ * <p>
+ * A version specifier has a branch name which identifies the respective branch.
+ * </p>
+ * 
+ * @author wesendon
+ * @author emueller
+ */
 public interface IVersionSpec {
 
+	/**
+	 * The factory used for creating {@link IVersionSpec}s.
+	 */
 	IVersionFactory FACTORY = VersionsFactory.INSTANCE;
 
 	String HEAD = "HEAD";
@@ -15,6 +29,11 @@ public interface IVersionSpec {
 	// magic global variable
 	String GLOBAL = "___GLOBAL___";
 
+	/**
+	 * Returns the branch name.
+	 * 
+	 * @return the branch name of this version specifier
+	 */
 	String getBranch();
 
 }
