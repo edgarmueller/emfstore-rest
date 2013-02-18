9daf61b4256697721834a19952e26f2c6028417a
Edgar Mueller <edgar.m.mueller@gmail.com>
2013-02-18 11:15:32 +0100

    added ExtensionRegistry to AllCommonTests test suite

--- org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/common/AllCommonTests.java
+++ org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/common/AllCommonTests.java
@@ -1,11 +1,12 @@
 package org.eclipse.emf.emfstore.client.test.common;
 
+import org.eclipse.emf.emfstore.client.test.common.extensionregistry.ExtensionRegistryTest;
 import org.eclipse.emf.emfstore.client.test.common.observerbus.ObserverBusTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ObserverBusTest.class })
+@Suite.SuiteClasses({ ObserverBusTest.class, ExtensionRegistryTest.class })
 public class AllCommonTests {
 
 }
