2755f4033241fb9736842dd47e2dda9a49618adc
Edgar Mueller <edgar.m.mueller@gmail.com>
2013-03-11 18:09:44 +0100

    added missing class

---
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/changeTracking/operations/SimpleOperationObserverTest.java
@@ -0,0 +1,57 @@
+package org.eclipse.emf.emfstore.client.test.changeTracking.operations;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.emf.emfstore.client.test.WorkspaceTest;
+import org.eclipse.emf.emfstore.internal.client.observers.SimpleOperationObserver;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.AbstractOperation;
+import org.junit.Test;
+
+public class SimpleOperationObserverTest extends WorkspaceTest {
+
+	boolean operationPerformed;
+
+	@Override
+	public void beforeHook() {
+		operationPerformed = false;
+	}
+
+	@Test
+	public void testForwards() {
+
+		getProjectSpace().getOperationManager().addOperationObserver(new SimpleOperationObserver() {
+
+			@Override
+			public void operationPerformed(AbstractOperation operation) {
+				operationPerformed = true;
+			}
+
+		});
+
+		getProject().addModelElement(createTestElement("A"));
+
+		assertTrue(operationPerformed);
+	}
+
+	@Test
+	public void testBackwards() {
+
+		getProject().addModelElement(createTestElement("A"));
+
+		assertFalse(operationPerformed);
+
+		getProjectSpace().getOperationManager().addOperationObserver(new SimpleOperationObserver() {
+
+			@Override
+			public void operationPerformed(AbstractOperation operation) {
+				operationPerformed = true;
+			}
+
+		});
+
+		getProjectSpace().revert();
+
+		assertTrue(operationPerformed);
+	}
+}
