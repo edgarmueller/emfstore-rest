4d38073bf2f993b514717a8a281ab708c8ea92d2
Edgar Mueller <edgar.m.mueller@gmail.com>
2013-04-16 22:35:53 +0200

    added additional test case

--- tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/changeTracking/recording/OperationRecorderTest.java
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/changeTracking/recording/OperationRecorderTest.java
@@ -10,14 +10,27 @@
  ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.changeTracking.recording;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.eclipse.emf.emfstore.client.test.WorkspaceTest;
 import org.eclipse.emf.emfstore.client.test.testmodel.TestElement;
+import org.eclipse.emf.emfstore.client.test.testmodel.TestElementContainer;
+import org.eclipse.emf.emfstore.client.test.testmodel.TestmodelFactory;
 import org.eclipse.emf.emfstore.client.util.RunESCommand;
 import org.eclipse.emf.emfstore.internal.client.model.impl.OperationRecorder;
+import org.eclipse.emf.emfstore.internal.common.model.ModelElementId;
 import org.eclipse.emf.emfstore.internal.common.model.Project;
 import org.eclipse.emf.emfstore.internal.common.model.impl.IdEObjectCollectionImpl;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.ChangePackage;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.VersioningFactory;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.AbstractOperation;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.CompositeOperation;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.CreateDeleteOperation;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -46,6 +59,7 @@ public class OperationRecorderTest extends WorkspaceTest {
 	 */
 	@Test
 	public void addElementWithDeletedReference() {
+
 		final Project project = getProject();
 
 		final TestElement element1 = getTestElement();
@@ -64,5 +78,50 @@ public class OperationRecorderTest extends WorkspaceTest {
 				return null;
 			}
 		});
+	}
+
+	/**
+	 * Test adding an element with deleted references.
+	 */
+	@Test
+	public void rescueElementAndDeleteIt() {
+
+		setCompareAtEnd(false);
+		// ExtensionRegistry.INSTANCE.set(ESOperationModifier.ID, new AutoOperationWrapper());
+
+		final Project project = getProject();
+
+		// final TestElement source = getTestElement("source");
+		final TestElement target = getTestElement("target");
+		final TestElement connection = getTestElement("connection");
+		final TestElementContainer source = TestmodelFactory.eINSTANCE.createTestElementContainer();
+
+		source.getElements().add(connection);
+		connection.setContainer(source);
+		target.getReferences().add(connection);
+
+		project.addModelElement(source);
+		ModelElementId connectionId = project.getModelElementId(connection);
+
+		ChangePackage cp = VersioningFactory.eINSTANCE.createChangePackage();
+		cp.getOperations().addAll(getProjectSpace().getOperations());
+		cp.apply(clonedProjectSpace.getProject());
+
+		// do not use commands since we only have them on client side
+		connection.setContainer(null);
+
+		List<AbstractOperation> ops = getProjectSpace().getOperations();
+		assertEquals(2, ops.size());
+		assertThat(ops.get(0), instanceOf(CompositeOperation.class));
+		assertThat(ops.get(1), instanceOf(CreateDeleteOperation.class));
+
+		// apply via change package, because change package does not start a command in contrast
+		// to projectSpace#applyOperations
+		cp = VersioningFactory.eINSTANCE.createChangePackage();
+		cp.getOperations().addAll(ops);
+		cp.apply(clonedProjectSpace.getProject());
+
+		// in case the connection could not be deleted because of a changed ID, we would expect 2 model elements
+		Assert.assertEquals(1, clonedProjectSpace.getProject().getModelElements().size());
 	}
 }
