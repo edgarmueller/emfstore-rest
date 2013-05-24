61376d61c00b94267e9ef1dfdc6b40fda0331aeb
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-05-24 16:54:28 +0200

    changed hudson config. try swtbot tests again

--- tests/org.eclipse.emf.emfstore.client.test.alltests/src/org/eclipse/emf/emfstore/client/test/alltests/AllSWTandNonSWTTests.java
+++ tests/org.eclipse.emf.emfstore.client.test.alltests/src/org/eclipse/emf/emfstore/client/test/alltests/AllSWTandNonSWTTests.java
@@ -12,6 +12,7 @@ package org.eclipse.emf.emfstore.client.test.alltests;
 
 import org.eclipse.emf.emfstore.client.test.AllTests;
 import org.eclipse.emf.emfstore.client.test.performance.AllPerformanceTests;
+import org.eclipse.emf.emfstore.client.test.ui.AllUITests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -22,14 +23,8 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author jfaltermeier
  * 
  */
-// @RunWith(Suite.class)
-// @SuiteClasses({ AllUITests.class, AllTests.class, AllPerformanceTests.class })
-// public class AllSWTandNonSWTTests {
-//
-// }
-
 @RunWith(Suite.class)
-@SuiteClasses({ AllTests.class, AllPerformanceTests.class })
+@SuiteClasses({ AllUITests.class, AllTests.class, AllPerformanceTests.class })
 public class AllSWTandNonSWTTests {
 
 }
