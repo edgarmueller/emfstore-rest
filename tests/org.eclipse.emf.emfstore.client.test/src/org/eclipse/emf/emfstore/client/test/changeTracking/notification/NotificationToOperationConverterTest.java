207715b61e7f8fc9a4fa03c3d7992dfe93267667
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-05-31 16:38:50 +0200

    missing headers + contributors in test plugins

--- tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/changeTracking/notification/NotificationToOperationConverterTest.java
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/changeTracking/notification/NotificationToOperationConverterTest.java
@@ -6,7 +6,8 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
+ * Contributors: 
+ * jsommerfeldt
  ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.changeTracking.notification;
 
--- tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/memory/MemoryLoadTest.java
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/memory/MemoryLoadTest.java
@@ -6,7 +6,8 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
+ * Contributors: 
+ * TobiasVerhoeven
  ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.memory;
 
