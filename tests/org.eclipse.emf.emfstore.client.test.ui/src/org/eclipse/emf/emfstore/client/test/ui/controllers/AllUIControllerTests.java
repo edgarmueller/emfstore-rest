5cb3bededb557389607f58068d5d2fbc2d4de803
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-05-31 11:16:22 +0200

    Replaced TUM with eclipsesource header. Added missing headers.

--- tests/org.eclipse.emf.emfstore.client.test.ui/src/org/eclipse/emf/emfstore/client/test/ui/controllers/AllUIControllerTests.java
+++ tests/org.eclipse.emf.emfstore.client.test.ui/src/org/eclipse/emf/emfstore/client/test/ui/controllers/AllUIControllerTests.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource Muenchen GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * Edgar
+ ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.ui.controllers;
 
 import org.junit.runner.RunWith;
