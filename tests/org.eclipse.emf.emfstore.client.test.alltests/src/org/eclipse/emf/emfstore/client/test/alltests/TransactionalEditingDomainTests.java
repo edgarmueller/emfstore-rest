10a433f7559b5f52e58d9d1ecbe55ede7c02f810
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-05-31 16:43:01 +0200

    test suite for transactional editing domain on hudson

---
+++ tests/org.eclipse.emf.emfstore.client.test.alltests/src/org/eclipse/emf/emfstore/client/test/alltests/TransactionalEditingDomainTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2013 EclipseSource Muenchen GmbH.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Johannes Faltermeier
+ ******************************************************************************/
+package org.eclipse.emf.emfstore.client.test.alltests;
+
+import org.eclipse.emf.emfstore.client.test.api.AllAPITests;
+import org.eclipse.emf.emfstore.client.test.changeTracking.AllChangeTrackingTests;
+import org.eclipse.emf.emfstore.client.test.ui.AllUITests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test suite for all SWT, API and ChangeTracking Tests. Used for testing with
+ * transactional editing domain on build server.
+ * 
+ * @author jfaltermeier
+ * 
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ AllAPITests.class, AllUITests.class, AllChangeTrackingTests.class })
+public class TransactionalEditingDomainTests {
+
+}
