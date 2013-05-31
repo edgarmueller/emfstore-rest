2e16dd3b435d0704bb571b2997d68c176edbb065
Julian Sommerfeldt <julian.sommerfeldt@googlemail.com>
2013-05-31 11:55:48 +0200

    added history compare feature

---
+++ bundles/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/ESCompare.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2011 Chair for Applied Software Engineering,
+ * Technische Universitaet Muenchen.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ ******************************************************************************/
+package org.eclipse.emf.emfstore.client.ui;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * Interface for extension point. This is used to register new comparators for
+ * the history browser.
+ * 
+ * @author groeber
+ * @author jsommerfeldt
+ */
+public interface ESCompare {
+	/**
+	 * Compares two EObjects to each other
+	 * 
+	 * @param e1
+	 *            EObject one
+	 * @param e2
+	 *            EObject two
+	 */
+	public void compare(EObject e1, EObject e2);
+
+	/**
+	 * Displays the compare result. This method is called if {@link #compare(EObject e1, EObject e2)} has been called
+	 * before and only then.
+	 */
+	public void display();
+}
---
+++ bundles/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/internal/client/ui/views/historybrowserview/EMFCompareComparator.java
@@ -0,0 +1,46 @@
+package org.eclipse.emf.emfstore.internal.client.ui.views.historybrowserview;
+
+import java.util.Calendar;
+
+import org.eclipse.compare.CompareUI;
+import org.eclipse.emf.compare.diff.metamodel.ComparisonResourceSnapshot;
+import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
+import org.eclipse.emf.compare.diff.service.DiffService;
+import org.eclipse.emf.compare.match.metamodel.MatchModel;
+import org.eclipse.emf.compare.match.service.MatchService;
+import org.eclipse.emf.compare.ui.editor.ModelCompareEditorInput;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.emfstore.client.ui.ESCompare;
+import org.eclipse.emf.emfstore.internal.common.model.Project;
+import org.eclipse.emf.emfstore.internal.common.model.util.ModelUtil;
+
+/**
+ * Implementation of {@link ESCompare} using the EMF Compare Framework to compare to {@link Project}s.
+ * 
+ * @author jsommerfeldt
+ * 
+ */
+public class EMFCompareComparator implements ESCompare {
+
+	private ComparisonResourceSnapshot snapshot;
+
+	public void compare(EObject e1, EObject e2) {
+		if (!(e1 instanceof Project) || !(e2 instanceof Project)) {
+			throw new IllegalArgumentException("The objects have to be Projects!");
+		}
+
+		try {
+			snapshot = DiffFactory.eINSTANCE.createComparisonResourceSnapshot();
+			snapshot.setDate(Calendar.getInstance().getTime());
+			MatchModel match = MatchService.doContentMatch(e1, e2, null);
+			snapshot.setMatch(match);
+			snapshot.setDiff(DiffService.doDiff(match, false));
+		} catch (InterruptedException e) {
+			ModelUtil.logException(e);
+		}
+	}
+
+	public void display() {
+		CompareUI.openCompareEditor(new ModelCompareEditorInput(snapshot), true);
+	}
+}
