e6bd81259be8cb5fda65d256a31cacb3dec4ec02
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-05-31 16:29:21 +0200

    reverse-merged from commit 5cb3bededb557389607f58068d5d2fbc2d4de803

--- bundles/org.eclipse.emf.emfstore.server/src/org/eclipse/emf/emfstore/internal/server/accesscontrol/authentication/AbstractAuthenticationControl.java
+++ bundles/org.eclipse.emf.emfstore.server/src/org/eclipse/emf/emfstore/internal/server/accesscontrol/authentication/AbstractAuthenticationControl.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 EclipseSource Muenchen GmbH.
- * 
+ * Copyright (c) 2008-2011 Chair for Applied Software Engineering,
+ * Technische Universitaet Muenchen.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: 
- * wesendonk
+ * Contributors:
  ******************************************************************************/
 package org.eclipse.emf.emfstore.internal.server.accesscontrol.authentication;
 
--- tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/common/extensionregistry/ExtensionRegistryTest.java
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/client/test/common/extensionregistry/ExtensionRegistryTest.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2013 EclipseSource Muenchen GmbH.
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- ******************************************************************************/
 package org.eclipse.emf.emfstore.client.test.common.extensionregistry;
 
 import static org.junit.Assert.assertEquals;
