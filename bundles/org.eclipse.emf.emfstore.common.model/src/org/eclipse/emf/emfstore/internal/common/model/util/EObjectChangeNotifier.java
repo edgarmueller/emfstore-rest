389e76cdf4376d3bb7abe50f0951f04208fbae66
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2013-07-02 10:37:02 +0200

    fixed handling of cut-elements

--- bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/internal/common/model/util/EObjectChangeNotifier.java
+++ bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/internal/common/model/util/EObjectChangeNotifier.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: 
+ * Contributors:
  * emueller
  * koegel
  ******************************************************************************/
@@ -188,7 +188,7 @@ public class EObjectChangeNotifier extends EContentAdapter {
 
 		// detect if the notification is about a reference to an object outside of the collection => notify
 		// project
-		if (feature instanceof EReference && newValue != null) {
+		if (feature instanceof EReference && newValue != null && !notification.isTouch()) {
 			EReference reference = (EReference) feature;
 
 			if (!reference.isContainment() && !reference.isContainer()) {
