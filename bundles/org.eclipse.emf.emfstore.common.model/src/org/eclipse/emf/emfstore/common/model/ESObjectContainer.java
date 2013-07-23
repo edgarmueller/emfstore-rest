23e5b31f5c2a0f6196faf11ec331569c3dd4cdc8
emueller <edgar.m.mueller@gmail.com>
2013-07-23 17:00:14 +0200

    bug 412646: Make use of API annotations, created API filter

--- bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/common/model/ESObjectContainer.java
+++ bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/common/model/ESObjectContainer.java
@@ -26,6 +26,8 @@ import org.eclipse.emf.ecore.EObject;
  * 
  * @author emueller
  * @author wesendon
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
  * 
  */
 public interface ESObjectContainer<T> {
--- bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/common/model/ESUniqueIdentifier.java
+++ bundles/org.eclipse.emf.emfstore.common.model/src/org/eclipse/emf/emfstore/common/model/ESUniqueIdentifier.java
@@ -18,6 +18,7 @@ package org.eclipse.emf.emfstore.common.model;
  * @author emueller
  * @author wesendon
  * 
+ * @noextend This interface is not intended to be extended by clients.
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ESUniqueIdentifier {
