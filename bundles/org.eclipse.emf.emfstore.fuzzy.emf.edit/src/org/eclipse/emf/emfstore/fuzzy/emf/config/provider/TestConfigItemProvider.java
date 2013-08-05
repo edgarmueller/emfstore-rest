12002f0ce79268dc3b7c0f97412b2ca1d2db44ef
Maximilian Koegel <maximilian@emfstore.org>
2013-08-05 19:08:26 +0200

    Bug 368670 - All plugins should have self-contained formatters, warning-settings and CS settings
    fixed all headers according to new headers

--- bundles/org.eclipse.emf.emfstore.fuzzy.emf.edit/src/org/eclipse/emf/emfstore/fuzzy/emf/config/provider/TestConfigItemProvider.java
+++ bundles/org.eclipse.emf.emfstore.fuzzy.emf.edit/src/org/eclipse/emf/emfstore/fuzzy/emf/config/provider/TestConfigItemProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 EclipseSource Muenchen GmbH.
+ * Copyright (c) 2012-2013 EclipseSource Muenchen GmbH and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
