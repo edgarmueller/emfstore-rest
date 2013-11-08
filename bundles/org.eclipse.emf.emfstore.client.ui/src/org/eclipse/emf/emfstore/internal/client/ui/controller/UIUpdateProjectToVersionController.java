08aaca6c00484213b6ff7d8b07c68e5b8b63c018
emueller <edgar.m.mueller@gmail.com>
2013-11-08 17:25:51 +0100

    bug 420822: Update to Version does not work in client ui https://bugs.eclipse.org/bugs/show_bug.cgi?id=420822, small fixed & javadoc

--- bundles/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/internal/client/ui/controller/UIUpdateProjectToVersionController.java
+++ bundles/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/internal/client/ui/controller/UIUpdateProjectToVersionController.java
@@ -8,7 +8,7 @@
  * 
  * Contributors:
  * Eugen Neufeld - initial API and implementation
- * 
+ * Edgar Mueller - fixes, javadoc
  *******************************************************************************/
 package org.eclipse.emf.emfstore.internal.client.ui.controller;
 
@@ -26,99 +26,101 @@ import org.eclipse.emf.emfstore.server.model.query.ESHistoryQuery;
 import org.eclipse.emf.emfstore.server.model.query.ESRangeQuery;
 import org.eclipse.emf.emfstore.server.model.versionspec.ESPrimaryVersionSpec;
 import org.eclipse.emf.emfstore.server.model.versionspec.ESVersionSpec;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.ListDialog;
 
 /**
- * UI controller for updating a project.
+ * UI controller for updating a project to a specific version.
  * 
  * @author eneufeld
+ * @author emueller
  */
 public class UIUpdateProjectToVersionController extends
 	AbstractEMFStoreUIController<ESPrimaryVersionSpec> {
 
 	private final ESLocalProject projectSpace;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param shell
+	 *            the parent {@link Shell}
+	 * @param localProject
+	 *            the {@link ESLocalProject} that should be updated to a certain version
+	 */
 	public UIUpdateProjectToVersionController(Shell shell, ESLocalProject localProject) {
 		super(shell, true, true);
-		this.projectSpace = localProject;
+		projectSpace = localProject;
 	}
 
 	@Override
 	public ESPrimaryVersionSpec doRun(IProgressMonitor monitor)
 		throws ESException {
-		ESRangeQuery query = ESHistoryQuery.FACTORY.rangeQuery(
+		final ESRangeQuery query = ESHistoryQuery.FACTORY.rangeQuery(
 			projectSpace.getBaseVersion(), 20, 0, false, false, false,
 			false);
-		try {
-			List<ESHistoryInfo> historyInfo = projectSpace.getHistoryInfos(query, new NullProgressMonitor());
-			// filter base version
-			Iterator<ESHistoryInfo> iter = historyInfo.iterator();
-			while (iter.hasNext()) {
-				if (projectSpace.getBaseVersion().equals(iter.next().getPrimarySpec())) {
-					iter.remove();
-					break;
-				}
+		final List<ESHistoryInfo> historyInfo = projectSpace.getHistoryInfos(query, new NullProgressMonitor());
+		// filter base version
+		final Iterator<ESHistoryInfo> iter = historyInfo.iterator();
+		while (iter.hasNext()) {
+			if (projectSpace.getBaseVersion().equals(iter.next().getPrimarySpec())) {
+				iter.remove();
+				break;
 			}
-			if (historyInfo.size() == 0) {
-				return new UIUpdateProjectController(
-					getShell(),
-					projectSpace,
-					ESVersionSpec.FACTORY.createHEAD(projectSpace.getBaseVersion()))
-					.execute();
-			}
+		}
+		if (historyInfo.size() == 0) {
+			return new UIUpdateProjectController(
+				getShell(),
+				projectSpace,
+				ESVersionSpec.FACTORY.createHEAD(projectSpace.getBaseVersion()))
+				.execute();
+		}
 
-			final ListDialog listDialog = createDialog(historyInfo);
-			Integer result = RunInUI.runWithResult(new Callable<Integer>() {
-				public Integer call() throws Exception {
-					return listDialog.open();
+		final ListDialog listDialog = createDialog(historyInfo);
+		final Integer result = RunInUI.runWithResult(new Callable<Integer>() {
+			public Integer call() throws Exception {
+				return listDialog.open();
+			}
+		});
+
+		if (Window.OK == result) {
+			final Object[] selection = listDialog.getResult();
+			final ESHistoryInfo info = (ESHistoryInfo) selection[0];
+			return RunInUI.WithException.runWithResult(new Callable<ESPrimaryVersionSpec>() {
+				public ESPrimaryVersionSpec call() throws Exception {
+					final UIUpdateProjectController controller = new UIUpdateProjectController(
+						getShell(),
+						projectSpace,
+						ESVersionSpec.FACTORY.createPRIMARY(
+							info.getPrimarySpec().getIdentifier()));
+					return controller.execute();
 				}
 			});
-
-			if (Dialog.OK == result) {
-				Object[] selection = listDialog.getResult();
-				final ESHistoryInfo info = (ESHistoryInfo) selection[0];
-				final UIUpdateProjectController controller = new UIUpdateProjectController(
-					getShell(),
-					projectSpace,
-					ESVersionSpec.FACTORY.createPRIMARY(
-						info.getPrimarySpec().getIdentifier()));
-				return RunInUI.WithException.runWithResult(new Callable<ESPrimaryVersionSpec>() {
-
-					public ESPrimaryVersionSpec call() throws Exception {
-						return controller.execute();
-					}
-				});
-			}
-		} catch (ESException e) {
-
 		}
 		return null;
 	}
 
 	private ListDialog createDialog(List<ESHistoryInfo> historyInfo) {
-		ListDialog listDialog = new ListDialog(getShell());
+		final ListDialog listDialog = new ListDialog(getShell());
 		listDialog.setContentProvider(ArrayContentProvider.getInstance());
 		listDialog.setLabelProvider(new LabelProvider() {
 
 			@Override
 			public String getText(Object element) {
 
-				ESHistoryInfo historyInfo = (ESHistoryInfo) element;
+				final ESHistoryInfo historyInfo = (ESHistoryInfo) element;
 
-				StringBuilder sb = new StringBuilder("Version ");
+				final StringBuilder sb = new StringBuilder("Version ");
 				sb.append(Integer.toString(historyInfo.getPrimarySpec()
 					.getIdentifier()));
 				sb.append("  -  ");
 				sb.append(historyInfo.getLogMessage().getMessage());
 
 				return sb.toString();
-
 			}
-
 		});
 		listDialog.setInput(historyInfo);
 		listDialog.setTitle("Select a Version to update to");
