277c49de7ee27badbf56029687e1b8512b03e9a6
Pascal Schliski <schliski@in.tum.de>
2014-01-28 20:59:47 +0100

    added Activator class

---
+++ org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/testOsgiConnector/Activator.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2014 EclipseSource Muenchen GmbH and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Pascal - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.emfstore.jaxrs.testOsgiConnector;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Pascal
+ * 
+ */
+public class Activator implements BundleActivator {
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
