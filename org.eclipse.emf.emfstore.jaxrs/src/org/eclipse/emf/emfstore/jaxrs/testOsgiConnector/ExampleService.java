35265e8b44d976742732c7b8494c46710fa9c1d3
Pascal Schliski <schliski@in.tum.de>
2014-01-30 17:03:08 +0100

    added POST method

--- org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/testOsgiConnector/ExampleService.java
+++ org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/testOsgiConnector/ExampleService.java
@@ -11,7 +11,10 @@
  ******************************************************************************/
 package org.eclipse.emf.emfstore.jaxrs.testOsgiConnector;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -33,5 +36,13 @@ public class ExampleService {
 	@Produces(MediaType.TEXT_HTML)
 	public String sayHelloHTML() {
 		return "<html> <body> <h1> JAX-RS and OSGi </h1> <p> are a lovely couple. </p> </body> </html>";
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String reply(@FormParam("arg") String arg) {
+
+		return "Your argument was: " + arg;
 	}
 }
