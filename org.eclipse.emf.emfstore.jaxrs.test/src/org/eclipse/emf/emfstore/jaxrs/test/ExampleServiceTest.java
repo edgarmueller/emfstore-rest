cb861983bb0410ba4ef54c86ed1cc946e5ea4a71
Pascal Schliski <schliski@in.tum.de>
2014-01-30 17:21:54 +0100

    added testSayHelloHTML

--- org.eclipse.emf.emfstore.jaxrs.test/src/org/eclipse/emf/emfstore/jaxrs/test/ExampleServiceTest.java
+++ org.eclipse.emf.emfstore.jaxrs.test/src/org/eclipse/emf/emfstore/jaxrs/test/ExampleServiceTest.java
@@ -8,21 +8,34 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class ExampleServiceTest {
-
+	
+	private static WebTarget target;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		Client client = ClientBuilder.newClient();
+		target = client.target("http://localhost:9090/services").path("osgi-jax-rs");
+	}
+	
 	@Test
 	public void testsayHelloPlain() {
-		
-				
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:9090/services").path("osgi-jax-rs");
-		
+	
 		Response response = target.request(MediaType.TEXT_PLAIN).get();
-		
+
 		assertEquals("JAX-RS and OSGi are a lovely couple.", response.readEntity(String.class));
+	}
+	
+	@Test
+	public void testsayHelloHTML() {
+		
+		Response response = target.request(MediaType.TEXT_HTML).get();
+
+		assertEquals("<html> <body> <h1> JAX-RS and OSGi </h1> <p> are a lovely couple. </p> </body> </html>", response.readEntity(String.class));
 	}
 
 }
