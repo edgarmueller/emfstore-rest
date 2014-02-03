d26e7865f40becdf336db119fe81b81be0236342
Pascal Schliski <schliski@in.tum.de>
2014-02-03 18:21:45 +0100

    further implemented getProjectList

--- org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/server/resources/Projects.java
+++ org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/server/resources/Projects.java
@@ -11,21 +11,30 @@
  ******************************************************************************/
 package org.eclipse.emf.emfstore.jaxrs.server.resources;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
 
-import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.emfstore.internal.server.EMFStore;
 import org.eclipse.emf.emfstore.internal.server.ServerConfiguration;
 import org.eclipse.emf.emfstore.internal.server.accesscontrol.AccessControl;
 import org.eclipse.emf.emfstore.internal.server.model.AuthenticationInformation;
 import org.eclipse.emf.emfstore.internal.server.model.ModelFactory;
+import org.eclipse.emf.emfstore.internal.server.model.ProjectInfo;
 import org.eclipse.emf.emfstore.internal.server.model.SessionId;
 import org.eclipse.emf.emfstore.server.exceptions.ESException;
 
@@ -50,26 +59,47 @@ public class Projects {
 		this.accessControl = accessControl;
 	}
 
+	@SuppressWarnings("restriction")
 	@GET
-	@Produces({ MediaType.TEXT_PLAIN })
+	@Produces({ MediaType.TEXT_XML })
+	// TODO: maybe application/xml instead?
 	public Response getProjectList() throws ESException {
 		// TODO implement!
 
 		// final EMFStore emfstore = null; not needed because we have class variable
 		// final AccessControl ac = null; not needed because we have class variable
 
+		// get a Session id //TODO: not RESTful!
 		final String superuser = ServerConfiguration.getProperties().getProperty(ServerConfiguration.SUPER_USER,
 			ServerConfiguration.SUPER_USER_DEFAULT);
 		final String superuserpw = ServerConfiguration.getProperties().getProperty(
 			ServerConfiguration.SUPER_USER_PASSWORD,
 			ServerConfiguration.SUPER_USER_PASSWORD_DEFAULT);
-		final AuthenticationInformation logIn = ac.logIn(superuser, superuserpw,
+		final AuthenticationInformation logIn = accessControl.logIn(superuser, superuserpw,
 			ModelFactory.eINSTANCE.createClientVersionInfo());
 		final SessionId sessionId = logIn.getSessionId();
 
-		emfStore.getProjectList(sessionId);
-		final XMLResource resource;
-		return Response.status(Status.NOT_IMPLEMENTED).build();
+		// get the projectList
+		final java.util.List<ProjectInfo> projects = emfStore.getProjectList(sessionId);
+
+		// convert the list into XML
+		final AdapterFactoryEditingDomain domain =
+			new AdapterFactoryEditingDomain(new AdapterFactoryImpl(), new BasicCommandStack());
+
+		final String fileNameURI = "blabla";
+		final XMLResourceImpl resource = (XMLResourceImpl) domain.createResource(fileNameURI);
+
+		final StreamingOutput streamingOutput = new StreamingOutput() {
+
+			public void write(OutputStream output) throws IOException, WebApplicationException {
+
+				resource.doSave(output, null);
+
+			}
+		};
+
+		// return the Response
+		return Response.ok(streamingOutput).build();
 	}
 
 	@GET
