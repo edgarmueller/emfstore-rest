63409b3f4b54b3cc97ee46e265ed2ee052736d90
Pascal Schliski <schliski@in.tum.de>
2014-02-03 17:01:52 +0100

    further implemented Projects.getProjectList

--- org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/server/resources/Projects.java
+++ org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/server/resources/Projects.java
@@ -20,6 +20,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.emfstore.internal.server.EMFStore;
+import org.eclipse.emf.emfstore.internal.server.ServerConfiguration;
+import org.eclipse.emf.emfstore.internal.server.accesscontrol.AccessControl;
+import org.eclipse.emf.emfstore.internal.server.model.AuthenticationInformation;
+import org.eclipse.emf.emfstore.internal.server.model.ModelFactory;
+import org.eclipse.emf.emfstore.internal.server.model.SessionId;
+import org.eclipse.emf.emfstore.server.exceptions.ESException;
+
 /**
  * @author Pascal
  * 
@@ -28,11 +37,38 @@ import javax.ws.rs.core.Response.Status;
 @Path("emfstore/projects")
 public class Projects {
 
+	private final EMFStore emfStore;
+	private final AccessControl accessControl;
+
+	/**
+	 * @param emfStore
+	 * @param accessControl
+	 */
+	public Projects(EMFStore emfStore, AccessControl accessControl) {
+		// TODO Auto-generated constructor stub
+		this.emfStore = emfStore;
+		this.accessControl = accessControl;
+	}
+
 	@GET
 	@Produces({ MediaType.TEXT_PLAIN })
-	public Response getProjectList() {
+	public Response getProjectList() throws ESException {
 		// TODO implement!
 
+		// final EMFStore emfstore = null; not needed because we have class variable
+		// final AccessControl ac = null; not needed because we have class variable
+
+		final String superuser = ServerConfiguration.getProperties().getProperty(ServerConfiguration.SUPER_USER,
+			ServerConfiguration.SUPER_USER_DEFAULT);
+		final String superuserpw = ServerConfiguration.getProperties().getProperty(
+			ServerConfiguration.SUPER_USER_PASSWORD,
+			ServerConfiguration.SUPER_USER_PASSWORD_DEFAULT);
+		final AuthenticationInformation logIn = ac.logIn(superuser, superuserpw,
+			ModelFactory.eINSTANCE.createClientVersionInfo());
+		final SessionId sessionId = logIn.getSessionId();
+
+		emfStore.getProjectList(sessionId);
+		final XMLResource resource;
 		return Response.status(Status.NOT_IMPLEMENTED).build();
 	}
 
