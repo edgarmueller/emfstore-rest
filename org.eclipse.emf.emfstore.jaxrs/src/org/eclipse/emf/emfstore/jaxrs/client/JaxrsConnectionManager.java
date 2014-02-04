ce25890107df9f20e875623bbe788b0d61982ecf
Pascal Schliski <schliski@in.tum.de>
2014-02-04 09:24:56 +0100

    implemented getProjectList in JaxrsConnectionManager

--- org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/client/JaxrsConnectionManager.java
+++ org.eclipse.emf.emfstore.jaxrs/src/org/eclipse/emf/emfstore/jaxrs/client/JaxrsConnectionManager.java
@@ -11,6 +11,8 @@
  ******************************************************************************/
 package org.eclipse.emf.emfstore.jaxrs.client;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.ws.rs.client.Client;
@@ -19,9 +21,13 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.emfstore.internal.common.model.Project;
 import org.eclipse.emf.emfstore.internal.server.model.ProjectId;
 import org.eclipse.emf.emfstore.internal.server.model.ProjectInfo;
+import org.eclipse.emf.emfstore.internal.server.model.impl.ProjectInfoImpl;
 import org.eclipse.emf.emfstore.internal.server.model.versioning.LogMessage;
 import org.eclipse.emf.emfstore.internal.server.model.versioning.VersionSpec;
 
@@ -43,10 +49,33 @@ public class JaxrsConnectionManager {
 	}
 
 	public List<ProjectInfo> getProjectList() {
-		// TODO: implement!!!
-		final Response response = target.path(PATH_PROJECTS).request(MediaType.TEXT_PLAIN).get();
 
-		return null;
+		final EditingDomain domain = AdapterFactoryEditingDomain.getEditingDomainFor(ProjectInfoImpl.class);
+		final String fileNameURI = "blabla";
+		final XMLResourceImpl resource = (XMLResourceImpl) domain.createResource(fileNameURI);
+
+		final Response response = target.path(PATH_PROJECTS).request(MediaType.TEXT_XML).get();
+		final InputStream is = response.readEntity(InputStream.class);
+		try {
+			resource.doLoad(is, null);
+			return (List<ProjectInfo>) resource.getAllContents();
+		} catch (final IOException ex) {
+			// TODO Auto-generated catch block
+			// Do NOT catch all Exceptions ("catch (Exception e)")
+			// Log AND handle Exceptions if possible
+			//
+			// You can just uncomment one of the lines below to log an exception:
+			// logException will show the logged excpetion to the user
+			// ModelUtil.logException(ex);
+			// ModelUtil.logException("YOUR MESSAGE HERE", ex);
+			// logWarning will only add the message to the error log
+			// ModelUtil.logWarning("YOUR MESSAGE HERE", ex);
+			// ModelUtil.logWarning("YOUR MESSAGE HERE");
+			//
+			// If handling is not possible declare and rethrow Exception
+		}
+
+		return null; // TODO: was ist best practice?
 	}
 
 	public Project getProject(ProjectId projectId, VersionSpec versionSpec) {
