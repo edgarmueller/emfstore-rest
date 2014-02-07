478232c806d6c1340a991470bb3986f44233a352
Pascal Schliski <schliski@in.tum.de>
2014-02-07 10:52:56 +0100

    solved URI resolvation problem

--- org.eclipse.emf.emfstore.jax.client/src/org/eclipse/emf/emfstore/jax/client/JaxrsConnectionManager.java
+++ org.eclipse.emf.emfstore.jax.client/src/org/eclipse/emf/emfstore/jax/client/JaxrsConnectionManager.java
@@ -21,6 +21,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
 import org.eclipse.emf.emfstore.internal.common.model.Project;
