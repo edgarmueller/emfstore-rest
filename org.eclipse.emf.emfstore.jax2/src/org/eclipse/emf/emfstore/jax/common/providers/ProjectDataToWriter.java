fd309e73f80659bbc138d161bf0a6ecd98a116b2
Pascal Schliski <schliski@in.tum.de>
2014-02-11 17:43:40 +0100

    changed ProjectDataToWriter

--- org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/providers/ProjectDataToWriter.java
+++ org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/providers/ProjectDataToWriter.java
@@ -5,13 +5,17 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
 
 import org.eclipse.emf.emfstore.jax.common.ProjectDataTO;
 
+@Provider
+@Produces({ MediaType.TEXT_XML })
 public class ProjectDataToWriter implements MessageBodyWriter<ProjectDataTO> {
 
 	public boolean isWriteable(Class<?> type, Type genericType,
@@ -22,7 +26,7 @@ public class ProjectDataToWriter implements MessageBodyWriter<ProjectDataTO> {
 
 	public long getSize(ProjectDataTO t, Class<?> type, Type genericType,
 			Annotation[] annotations, MediaType mediaType) {
-		// TODO Auto-generated method stub
+		// deprecated since Jersey 2.0
 		return 0;
 	}
 
