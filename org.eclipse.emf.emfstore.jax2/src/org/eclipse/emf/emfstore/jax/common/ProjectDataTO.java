544e8d919659cc24c9608eed7deed1b81466e1e4
Pascal Schliski <schliski@in.tum.de>
2014-02-13 12:09:07 +0100

    implemented EObject Serialization in ProjectDataTO

--- org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/ProjectDataTO.java
+++ org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/ProjectDataTO.java
@@ -1,9 +1,19 @@
 package org.eclipse.emf.emfstore.jax.common;
 
+import java.util.HashMap;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.emfstore.internal.common.ResourceFactoryRegistry;
 import org.eclipse.emf.emfstore.internal.common.model.Project;
 import org.eclipse.emf.emfstore.internal.common.model.util.ModelUtil;
 import org.eclipse.emf.emfstore.internal.common.model.util.SerializationException;
 import org.eclipse.emf.emfstore.internal.server.model.versioning.LogMessage;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -14,6 +24,11 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class ProjectDataTO {
+	
+	/**
+	 * URI used to serialize EObject with the model util.
+	 */
+	public static final URI VIRTUAL_URI = URI.createURI("virtualUri");
 	
 	private String name;
 	private String description;
@@ -40,34 +55,39 @@ public class ProjectDataTO {
 		
 		try {
 			if(logMessage != null) {
-				this.logMessage = ModelUtil.eObjectToString(logMessage);
+	//			this.logMessage = ModelUtil.eObjectToString(logMessage);
+				this.logMessage = serializeEObjectToString(logMessage);
 			} if(project != null) {
-				this.project = ModelUtil.eObjectToString(project);
+	//			this.project = ModelUtil.eObjectToString(project);
+				this.project = serializeEObjectToString(project);
 			}
 		} catch (SerializationException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
-	/**
-	 * @param name
-	 * @param description
-	 * @param logMessage
-	 */
-	public ProjectDataTO(String name, String description, LogMessage logMessage) {
-		super();
-		this.name = name;
-		this.description = description;
-		try {
-			if(logMessage != null) {
-				this.logMessage = ModelUtil.eObjectToString(logMessage);
-			}
-		} catch (SerializationException e) {
-			e.printStackTrace();
-		}
-		this.project = null;
-	}
+//	/**
+//	 * @param name
+//	 * @param description
+//	 * @param logMessage
+//	 */
+//	public ProjectDataTO(String name, String description, LogMessage logMessage) {
+//		super();
+//		this.name = name;
+//		this.description = description;
+//		try {
+//			if(logMessage != null) {
+//				this.logMessage = ModelUtil.eObjectToString(logMessage);
+//			}
+//		} catch (SerializationException e) {
+//			e.printStackTrace();
+//		}
+//		this.project = null;
+//	}
 	
+	
+
 	public String getName() {
 		return name;
 	}
@@ -106,5 +126,37 @@ public class ProjectDataTO {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	
+	/**
+	 * converts an EObject to a String as XML without beeing a complete XML-Document (i. e. no documentType header)
+	 * @param object
+	 * @return
+	 * @throws SerializationException
+	 */
+	private String serializeEObjectToString(EObject object) throws SerializationException {
+		
+		if (object == null) {
+			return null;
+		}
+
+		//create a XMLResource and convert the eObject ot a String 
+		final ResourceSetImpl resourceSetImpl = new ResourceSetImpl();
+		resourceSetImpl.setResourceFactoryRegistry(new ResourceFactoryRegistry());
+		final XMIResource res = (XMIResource) resourceSetImpl.createResource(VIRTUAL_URI);
+		((ResourceImpl) res).setIntrinsicIDToEObjectMap(new HashMap<String, EObject>());
+		
+		String resultFullXmlDoc = ModelUtil.copiedEObjectToString(object, res);
+		
+		//remove the xml doc declaration
+		String xmlDocDecl = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		int lastIndexOfXmlDocDeclaration = resultFullXmlDoc.lastIndexOf(xmlDocDecl);
+		String result = resultFullXmlDoc.substring(lastIndexOfXmlDocDeclaration + xmlDocDecl.length() + 1).trim();
+		
+		//TODO: Remove debug println!
+		System.out.println("\n\nProjectDataTO.serializeEObjectToString result:\n" + result + "\n\n");
+		
+		return result;
 	}
 }
