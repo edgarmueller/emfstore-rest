05a410b0354f1120eb8571e2578661297c6e36f9
Pascal Schliski <schliski@in.tum.de>
2014-02-14 16:41:48 +0100

    extended CallParamStrings

--- org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/CallParamStrings.java
+++ org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/CallParamStrings.java
@@ -7,6 +7,15 @@ package org.eclipse.emf.emfstore.jax.common;
  */
 public interface CallParamStrings {
 	
+	//Path param constants  //TODO: use these params + do create more!!!
+	public static final String PROJECT_ID_PATH_PARAM = "projectId";
+	
+	//Base uri where the services are published
 	public final static String BASE_URI = "http://localhost:9090/services";
+	
+	//path constants
 	public final static String PROJECTS_PATH = "projects";
+	public final static String BRANCHES_PATH = PROJECTS_PATH + "/" + "{" + PROJECT_ID_PATH_PARAM + "}" + "branches";
+	
+	
 }
