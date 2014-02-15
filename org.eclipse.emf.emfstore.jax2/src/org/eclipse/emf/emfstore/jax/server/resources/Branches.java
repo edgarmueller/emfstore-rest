3be4566ec7aa2d0e3207bfae27e36c6655fabc1e
Pascal Schliski <schliski@in.tum.de>
2014-02-15 15:54:00 +0100

    implemented Branches.getBranches

--- org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/server/resources/Branches.java
+++ org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/server/resources/Branches.java
@@ -1,18 +1,43 @@
 package org.eclipse.emf.emfstore.jax.server.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
 
+import org.eclipse.emf.emfstore.internal.server.model.ModelFactory;
+import org.eclipse.emf.emfstore.internal.server.model.ProjectId;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.BranchInfo;
 import org.eclipse.emf.emfstore.jax.common.CallParamStrings;
+import org.eclipse.emf.emfstore.server.exceptions.ESException;
 
 @Path(CallParamStrings.BRANCHES_PATH)
 public class Branches extends JaxrsResource {
 	
 	@GET
-	public Response getTest() {
-		return Response.ok("It works!").build();
+	@Produces({ MediaType.TEXT_XML })
+	public Response getBranches(@PathParam("projectId") String projectIdAsString) {
+		
+		//create ProjectId
+		ProjectId projectId = ModelFactory.eINSTANCE.createProjectId();
+		projectId.setId(projectIdAsString);
+		
+		try {
+			List<BranchInfo> branches = emfStore.getBranches(retrieveSessionId(), projectId );
+			
+			final StreamingOutput streamingOutput = convertEObjectsToXmlIntoStreamingOutput(branches);
+			return Response.ok(streamingOutput).build();
+			
+		} catch (ESException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 
 }
