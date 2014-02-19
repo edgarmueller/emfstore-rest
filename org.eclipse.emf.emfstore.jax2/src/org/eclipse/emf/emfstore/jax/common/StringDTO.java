73ade9b85fd0e97599c2d2c23966c3744bb48eda
Pascal Schliski <schliski@in.tum.de>
2014-02-19 08:54:13 +0100

    created StringDTO

---
+++ org.eclipse.emf.emfstore.jax2/src/org/eclipse/emf/emfstore/jax/common/StringDTO.java
@@ -0,0 +1,154 @@
+package org.eclipse.emf.emfstore.jax.common;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+
+/**
+ * wrapper class for Strings as EObjects
+ * @author Pascal
+ *
+ */
+public class StringDTO implements EObject {
+	
+	private String string;
+
+	/**
+	 * @param string
+	 */
+	public StringDTO(String string) {
+		super();
+		this.string = string;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	@Override
+	public EList<Adapter> eAdapters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eDeliver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eSetDeliver(boolean deliver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eNotify(Notification notification) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EClass eClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource eResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EObject eContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EStructuralFeature eContainingFeature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EReference eContainmentFeature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EList<EObject> eContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeIterator<EObject> eAllContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eIsProxy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public EList<EObject> eCrossReferences() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object eGet(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object eGet(EStructuralFeature feature, boolean resolve) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eSet(EStructuralFeature feature, Object newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean eIsSet(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eUnset(EStructuralFeature feature) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object eInvoke(EOperation operation, EList<?> arguments)
+			throws InvocationTargetException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
